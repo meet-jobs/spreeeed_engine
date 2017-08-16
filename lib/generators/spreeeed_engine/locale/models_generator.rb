8551cb8813156b835185fabfafa6d61533e28590
MidnightBlue <isfore@gmail.com>
2017-08-16 16:01:32 +0800

    locale generators

--- lib/generators/spreeeed_engine/locale/models_generator.rb
+++ lib/generators/spreeeed_engine/locale/models_generator.rb
@@ -1,36 +1,37 @@
 module SpreeeedEngine
   module Locales
-    class ModelsGenerator < Rails::Generators::Base
+    module Generators
+      class ModelsGenerator < Rails::Generators::Base
 
-      desc <<-DESC.strip_heredoc
-        Generate all locale files of models (app-scope)
+        desc <<-DESC.strip_heredoc
+          Generate all locale files of models (app-scope)
 
-        For example:
-          rails generate spreeeed_engine:locales:models LANG
+          For example:
+            rails generate spreeeed_engine:locales:models LANG
 
-          This will create:
-            config/locales/models/*/*/LANG.yml
+            This will create:
+              config/locales/models/*/*/LANG.yml
 
-          real world case:
-            rails g spreeeed_engine:locales:models zh-TW
+            real world case:
+              rails g spreeeed_engine:locales:models zh-TW
 
-          then you can find local file in 
-            config/locales/models/user/zh-TW.yml
-            config/locales/models/article/zh-TW.yml
-            ...
+            then you can find local file in 
+              config/locales/models/user/zh-TW.yml
+              config/locales/models/article/zh-TW.yml
+              ...
 
-      DESC
+        DESC
 
-      argument :lang, type: :string, default: 'en'
+        argument :lang, type: :string, default: 'en'
 
-      def create_locale_files
-        Rails.application.eager_load!
-        model_names = ApplicationRecord.descendants.map(&:name)
-        model_names.each do |model_name|
-          generate "locales:model #{model_name} #{lang}"
+        def create_locale_files
+          Rails.application.eager_load!
+          model_names = ApplicationRecord.descendants.map(&:name)
+          model_names.each do |model_name|
+            generate "locales:model #{model_name} #{lang}"
+          end
         end
       end
-
     end
   end
 end
