7ff3e3356d256d7bf133acd961d879d41b1523b8
MidnightBlue <isfore@gmail.com>
2017-08-16 15:55:15 +0800

    locale generators

---
+++ lib/generators/spreeeed_engine/locale/model_generator.rb
@@ -0,0 +1,47 @@
+module SpreeeedEngine
+  module Locales
+    class ModelGenerator < Rails::Generators::NamedBase
+      
+      desc <<-DESC.strip_heredoc
+        Generate locale file of a single model
+
+        For example:
+          rails generate spreeeed_engine:locales:model NAME LANG
+
+          This will create:
+            config/locales/models/NAME/LANG.yml
+
+          real world case:
+            rails g spreeeed_engine:locales:model user zh-TW
+
+          then you can find local file in 
+            config/locales/models/user/zh-TW.yml
+
+          and all attributes will be preset in YAML file, you just need
+          to translate them.
+
+          This generator also support model with namespace, say
+            rails g spreeeed_engine:locales:model user::address zh-TW
+
+          then you can find local file in 
+            config/locales/models/user/address/zh-TW.yml
+
+      DESC
+
+      source_root File.expand_path('../templates', __FILE__)
+
+      argument :lang, type: :string, default: 'en'
+
+      # http://api.rubyonrails.org/classes/ActiveSupport/Inflector.html
+      def create_locale_file
+        klass       = name.titleize.classify.constantize
+        @model_name = name.titleize.singularize.downcase
+        @columns    = klass.column_names
+        template 'en.yml',
+                 "config/locales/models/#{@model_name}/#{lang}.yml"
+        # assigns: { model_name: @model_name, lang: lang, columns: @columns }
+      end
+    end
+  end
+end
+
---
+++ lib/generators/spreeeed_engine/locale/models_generator.rb
@@ -0,0 +1,36 @@
+module SpreeeedEngine
+  module Locales
+    class ModelsGenerator < Rails::Generators::Base
+
+      desc <<-DESC.strip_heredoc
+        Generate all locale files of models (app-scope)
+
+        For example:
+          rails generate spreeeed_engine:locales:models LANG
+
+          This will create:
+            config/locales/models/*/*/LANG.yml
+
+          real world case:
+            rails g spreeeed_engine:locales:models zh-TW
+
+          then you can find local file in 
+            config/locales/models/user/zh-TW.yml
+            config/locales/models/article/zh-TW.yml
+            ...
+
+      DESC
+
+      argument :lang, type: :string, default: 'en'
+
+      def create_locale_files
+        Rails.application.eager_load!
+        model_names = ApplicationRecord.descendants.map(&:name)
+        model_names.each do |model_name|
+          generate "locales:model #{model_name} #{lang}"
+        end
+      end
+
+    end
+  end
+end
