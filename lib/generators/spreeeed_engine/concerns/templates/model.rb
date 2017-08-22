47cedc004da6ab2738a9247f569177302b5d16a7
MidnightBlue <isfore@gmail.com>
2017-08-22 15:23:25 +0800

    generate spreeeed_engine concerns files

---
+++ lib/generators/spreeeed_engine/concerns/templates/model.rb
@@ -0,0 +1,19 @@
+module SpreeeedEngine
+  module Models
+<% @namespaces.each_with_index do |namespace, i| %><%= '  ' * (i+2) %>module <%= namespace.titleize.classify %><%= "\n" %><% end %>
+<%= @indent %>extend ActiveSupport::Concern
+
+<%= @indent %>class_methods do
+  <%= @indent %>def icon
+    <%= @indent %>'fa-edit'
+  <%= @indent %>end
+<% @methods.each do |method| %>
+  <%= @indent %>def <%= method.to_s %>
+    <%= @indent %><%= @klass.send(method).to_s %>
+  <%= @indent %>end
+<% end %>
+<%= @indent %>end
+
+<% @namespaces.enum_for(:each_with_index).collect { |namespace, i| "#{'  ' * (i+2)}end\n" }.reverse.each { |end_line| %><%= end_line %><% } %>
+  end
+end
