0936ab6c953cbaed0f9e0380363dc47fa3ca5111
MidnightBlue <isfore@gmail.com>
2018-05-14 19:18:50 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-SpreeeedEngine::Engine.routes.draw do
-
-  root to: 'scratch#hello'
-  get 'scratch/index', to: 'scratch#index'
-  get 'scratch/typeahead_data', to: 'scratch#typeahead_data'
-
-  resources :fake_objects
-  resources :fake_photos
-
-end
+# SpreeeedEngine::Engine.routes.draw do
+#
+#   root to: 'scratch#hello'
+#   get 'scratch/index', to: 'scratch#index'
+#   get 'scratch/typeahead_data', to: 'scratch#typeahead_data'
+#
+#   resources :fake_objects
+#   resources :fake_photos
+#
+# end
